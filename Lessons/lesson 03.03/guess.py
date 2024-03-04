560cd704e2ec2ff7c5fe7e6a21a681171d38ee1f
Mikita Ivanow <nickita_02@mail.ru>
2024-03-04 19:44:59 +0300

    Tasks
    Delete unnecessary files

--- Lessons/lesson 03.03/guess.py
+++
@@ -1,9 +0,0 @@
-import random
-
-my_number = random.randint(1, 10)
-your_number = int(input("Guess an integer number from 1 to 10: "))
-
-if my_number == your_number:
-    print("Lucky guess!")
-else:
-    print("Loser!")
