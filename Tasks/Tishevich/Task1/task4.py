e2db7d58f1aba5c877dcee3a50747f36197b94b4
Katherine Tishevich <61135659+katherinetishevich@users.noreply.github.com>
2024-03-13 19:15:17 +0300

    Update task4.py

--- Tasks/Tishevich/Task1/task4.py
+++ Tasks/Tishevich/Task1/task4.py
@@ -1,7 +1,7 @@
 # Write a program that converts some amount of money from USD to BYN, ask a user for the amount, store the ratio inside
 # the program itself.
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 
 usd_count = float(input("Enter USD amount: "))
