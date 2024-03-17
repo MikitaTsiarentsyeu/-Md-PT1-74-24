a3944d31265822445f3045cc261b3f620271e919
NsDrai <gladun.vladimir03@mail.ru>
2024-03-17 16:52:03 +0300

    completed_task_1

---
+++ Tasks/Gladun/task1/task2.py
@@ -0,0 +1,2 @@
+km_h = float(input("enter speed in km/h: " ))
+print("your speed in m/s: " , km_h / 3.6)
---
+++ Tasks/Gladun/task1/task3.py
@@ -0,0 +1,2 @@
+USD = float(input("entered amount in USD: "))
+print("amount in BYN: " , USD * 3.27 )
