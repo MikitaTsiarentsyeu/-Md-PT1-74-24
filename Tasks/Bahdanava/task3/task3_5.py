30b4d1ac09025dd6ee9064c71d294b027060a2e9
anasbahdanava <anabahdanava@gmail.com>
2024-03-18 18:07:50 +0100

    Version2

---
+++ Tasks/Bahdanava/task3/task3_5.py
@@ -0,0 +1,12 @@
+def string_length(string):
+
+    new_string = []
+    for i in string:
+        if len(i) > 5:
+            new_string.append(i)
+    print(new_string)
+
+string = ['Today', 'is', 'an', 'awesome', 'day', 'spring', 'started']
+string_length(string)
+
+
