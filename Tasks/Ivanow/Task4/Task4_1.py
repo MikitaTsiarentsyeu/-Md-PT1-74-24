28bb4a8fa4d1ec1a3cc05cf59de006815fb406bc
Mikita Ivanow <nickita_02@mail.ru>
2024-04-12 18:53:15 +0300

    started task4

---
+++ Tasks/Ivanow/Task4/Task4_1.py
@@ -0,0 +1,13 @@
+#c_max = int(input('Input maximum chars per line:\n'))
+c_max = 10
+f = open("D:\\python\\github\\Md-PT1-74-24\\Tasks\\!!!Tasks\\Task4\\text.txt")
+
+new_text = ''
+word = ''
+count = 0
+
+for line in f:
+        for c in line:
+            
+
+print(new_text)
