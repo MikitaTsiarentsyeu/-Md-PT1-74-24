1e3a6aa91f0db8003376370bf86e7276736973f5
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2024-05-04 11:45:33 +0300

    add lesson 04/05

--- Lessons/web project/proj/main/views.py
+++ Lessons/web project/proj/main/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 import datetime
+from .models import Post, Author
 
 # Create your views here.
-def test(request):
+def test(request, post_id=0):
     # print("test view")
+    print(post_id, type(post_id))
     t = datetime.datetime.now()
     return render(request, "test.html", {"current_time":t})
+
+def show_posts(request):
+    posts = Post.objects.all()
+
+    return render(request, "posts.html", {"posts":posts})
+
+def show_post(request, post_id):
+    try:
+        p = Post.objects.get(id=post_id)
+    except:
+        return HttpResponseNotFound("<h1>404 - Post not found</h1>")
+
+    return render(request, "post.html", {"post":p})
