f53cb51d99f3c50d8ed8d3bd691336fa8703e033
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2024-05-11 11:45:05 +0300

    add lesson 11/05

--- Lessons/web project/proj/main/views.py
+++ Lessons/web project/proj/main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 import datetime
 from .models import Post, Author
+from .forms import AddPost
 
 # Create your views here.
 def test(request, post_id=0):
@@ -22,3 +23,28 @@ def show_post(request, post_id):
         return HttpResponseNotFound("<h1>404 - Post not found</h1>")
 
     return render(request, "post.html", {"post":p})
+
+def add_post(request):
+
+    if request.method == 'POST':
+        add_post_form = AddPost(request.POST, request.FILES)
+
+        if add_post_form.is_valid():
+            post_entry = Post()
+            post_entry.author = Author.objects.all()[0]
+            post_entry.issued = datetime.datetime.now()
+            post_entry.title = add_post_form.cleaned_data['title']
+            post_entry.content = add_post_form.cleaned_data['content']
+            post_entry.post_type = add_post_form.cleaned_data['post_type']
+            post_entry.image = add_post_form.cleaned_data['image']
+
+            post_entry.save()
+
+            return redirect('post', post_entry.id)
+
+
+
+    else:
+        add_post_form = AddPost()
+
+    return render(request, "add_post.html", {'form':add_post_form})
