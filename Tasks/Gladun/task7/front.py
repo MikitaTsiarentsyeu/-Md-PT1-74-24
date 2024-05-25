d0e4a9ff3bc100887f06b05f59e9c2d91fb8d72d
NsDrai <gladun.vladimir03@mail.ru>
2024-05-25 13:49:47 +0300

    complite_task_7

--- Tasks/Gladun/task7/front.py
+++ Tasks/Gladun/task7/front.py
@@ -1,22 +1,32 @@
 import back
 
+def show():
+    try:
+        print(back.show())
+    except:
+        print("show error")
+
+def add():
+    values = []
+    for param in back.params:
+        value = input(f"please, insert {param}:\n")
+        values.append(value)
+    back.add(*values)
+    print("file added")
 def start():
     while True:
         user_input = input(""" Commands:
                         0. Exit
                         1. Show all
-                        2. Add file
-                        3. Search title
+                        2. add file
         """)
 
-        if user_input == 0:
+        if user_input == '0':
             print("Have nice day")
             break
-        elif user_input == 1:
+        elif user_input == '1':
             show()
-        elif user_input == 2:
-            add_file()
-        elif user_input == 3:
-            searth()
+        elif user_input == '2':
+            add()
         else:
             print("Unknown operation, please try again")
